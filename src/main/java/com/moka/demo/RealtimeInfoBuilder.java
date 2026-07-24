2983911ba1bcb951e2bc104a7ecc4c789feb7585
“caimuhan” <cmh040228@163.com>
2026-07-24 18:33:41 +0800

    chore: remove @Profile from DemoController/RealtimeInfoBuilder, keep on ProxyController only

--- src/main/java/com/moka/demo/RealtimeInfoBuilder.java
+++ src/main/java/com/moka/demo/RealtimeInfoBuilder.java
@@ -8,7 +8,6 @@ import com.moka.ai.retrieval.RestaurantRepository;
 import com.moka.ai.tools.WeatherTool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -30,7 +29,6 @@ import java.util.Optional;
  * 3. 调 timor.tech API 判断节假日
  */
 @Component
-@Profile("demo")
 public class RealtimeInfoBuilder {
 
     private static final Logger log = LoggerFactory.getLogger(RealtimeInfoBuilder.class);
